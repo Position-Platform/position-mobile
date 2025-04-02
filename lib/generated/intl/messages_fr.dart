7f39fbbccc3bcc67cee701580b2781074ad1823e
TCHOUKOUAHA Boris Gautier <btchoukouaha@univ-catho-sjd.com>
2025-04-02 10:03:41 +0100

    fix(localization): Update French and English messages for consistency and clarity

--- lib/generated/intl/messages_fr.dart
+++ lib/generated/intl/messages_fr.dart
@@ -22,139 +22,148 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-    "addEmail": MessageLookupByLibrary.simpleMessage(
-      "Remplissez l\'adresse mail",
-    ),
-    "alreadyAccount": MessageLookupByLibrary.simpleMessage(
-      "J\'ai déja un compte",
-    ),
-    "appname": MessageLookupByLibrary.simpleMessage("Position"),
-    "askAccess": MessageLookupByLibrary.simpleMessage("Demandez l\'access"),
-    "close": MessageLookupByLibrary.simpleMessage("Fermé : "),
-    "closed": MessageLookupByLibrary.simpleMessage("Fermé"),
-    "confirmPassword": MessageLookupByLibrary.simpleMessage(
-      "Confirmer le Mot de Passe",
-    ),
-    "connexion": MessageLookupByLibrary.simpleMessage("Connexion"),
-    "cpassword": MessageLookupByLibrary.simpleMessage(
-      "Confirmer le Mot de Passe",
-    ),
-    "createAccount": MessageLookupByLibrary.simpleMessage("Creer un compte"),
-    "email": MessageLookupByLibrary.simpleMessage("Adresse Mail"),
-    "emailNoSend": MessageLookupByLibrary.simpleMessage(
-      "Erreur de mail. Reessayez!!!",
-    ),
-    "emailSend": MessageLookupByLibrary.simpleMessage(
-      "Un mail de reinitialisation vous a été envoyé",
-    ),
-    "enableGps": MessageLookupByLibrary.simpleMessage(
-      "Assurez-vous d\'activer le GPS.",
-    ),
-    "enterEmailForReset": MessageLookupByLibrary.simpleMessage(
-      "Saisir l\'e-mail pour réinitialiser",
-    ),
-    "error": MessageLookupByLibrary.simpleMessage("Une erreur est survenue"),
-    "forgotPass": MessageLookupByLibrary.simpleMessage("Mot de Passe oublié ?"),
-    "go": MessageLookupByLibrary.simpleMessage("Allez-y"),
-    "gpsAccess": MessageLookupByLibrary.simpleMessage(
-      "Un accès GPS est nécessaire",
-    ),
-    "hintIdText": MessageLookupByLibrary.simpleMessage(
-      "Entrez votre identifiant ou votre email",
-    ),
-    "hintPasswordText": MessageLookupByLibrary.simpleMessage(
-      "Entrez votre mot de passe",
-    ),
-    "hintSearch": MessageLookupByLibrary.simpleMessage("Rechercher"),
-    "invalidId": MessageLookupByLibrary.simpleMessage(
-      "Téléphone ou Email Invalide",
-    ),
-    "invalidMail": MessageLookupByLibrary.simpleMessage("Email Invalide"),
-    "invalidPass": MessageLookupByLibrary.simpleMessage(
-      "Mot de Passe Invalide",
-    ),
-    "invalidPhone": MessageLookupByLibrary.simpleMessage("Téléphone Invalide"),
-    "labelIdText": MessageLookupByLibrary.simpleMessage("Identifiant/Email"),
-    "labelPasswordText": MessageLookupByLibrary.simpleMessage("Mot de passe"),
-    "loading": MessageLookupByLibrary.simpleMessage("Chargement..."),
-    "loggin": MessageLookupByLibrary.simpleMessage("Connexion..."),
-    "login": MessageLookupByLibrary.simpleMessage("CONNECTEZ VOUS"),
-    "loginFailed": MessageLookupByLibrary.simpleMessage("Echec de Connexion"),
-    "loginSuccess": MessageLookupByLibrary.simpleMessage("Connexion reussie"),
-    "maintenance": MessageLookupByLibrary.simpleMessage(
-      "Votre application est en cours de maintenance. Reessayez plus tard",
-    ),
-    "next": MessageLookupByLibrary.simpleMessage("Suivant"),
-    "noAccount": MessageLookupByLibrary.simpleMessage(
-      "Pas encore de Compte?  ",
-    ),
-    "noInternet": MessageLookupByLibrary.simpleMessage(
-      "Pas de connexion internet",
-    ),
-    "open": MessageLookupByLibrary.simpleMessage("Ouvert"),
-    "opennow": MessageLookupByLibrary.simpleMessage("Ouvert Maintenant : "),
-    "or": MessageLookupByLibrary.simpleMessage("- OU -"),
-    "passError": MessageLookupByLibrary.simpleMessage(
-      "Mots de passes differents",
-    ),
-    "password": MessageLookupByLibrary.simpleMessage("Mot de Passe"),
-    "passwordReset": MessageLookupByLibrary.simpleMessage(
-      "réinitialisation du mot de passe",
-    ),
-    "passwordsDontMatch": MessageLookupByLibrary.simpleMessage(
-      "Les mots de passe ne correspondent pas",
-    ),
-    "phone": MessageLookupByLibrary.simpleMessage("Numéro de Téléphone"),
-    "register": MessageLookupByLibrary.simpleMessage("S\'INSCRIRE"),
-    "registerFailed": MessageLookupByLibrary.simpleMessage(
-      "Echec de création de compte",
-    ),
-    "registerSuccess": MessageLookupByLibrary.simpleMessage(
-      "Création du compte reussie verifiez vos mails...",
-    ),
-    "registering": MessageLookupByLibrary.simpleMessage(
-      "Création du compte...",
-    ),
-    "resetPassword": MessageLookupByLibrary.simpleMessage(
-      "Reinitialiser le Mot de Passe",
-    ),
-    "resetsuccess": MessageLookupByLibrary.simpleMessage(
-      "reinitialisation effectuée",
-    ),
-    "search": MessageLookupByLibrary.simpleMessage(
-      "Rechercher un établissement, un lieu...",
-    ),
-    "searcherror": MessageLookupByLibrary.simpleMessage("Erreur de Chargement"),
-    "searchnotfound": MessageLookupByLibrary.simpleMessage(
-      "Aucun resultat trouvé",
-    ),
-    "sendResetLink": MessageLookupByLibrary.simpleMessage(
-      "Envoyer un lien de reinitialisation",
-    ),
-    "serverError": MessageLookupByLibrary.simpleMessage(
-      "Une erreur est survenue",
-    ),
-    "signwith": MessageLookupByLibrary.simpleMessage("Se Connecter avec"),
-    "skip": MessageLookupByLibrary.simpleMessage("Passer"),
-    "subtitle1": MessageLookupByLibrary.simpleMessage(
-      "Impression de plan officiel de localisation, partage, enregistrement dans ses favoris ou naviguation d\'un endroit vers un autre Pharmacie de garde, banque, centre de santé, écoles, infrastructures sociales et services publics",
-    ),
-    "subtitle2": MessageLookupByLibrary.simpleMessage(
-      "Une boutique virtuelle et un marché digital pour les activités formelles et informelles ...",
-    ),
-    "subtitle3": MessageLookupByLibrary.simpleMessage(
-      "Déclarations de Travaux, suivre l’actualité locale via les avis et annonces",
-    ),
-    "title1": MessageLookupByLibrary.simpleMessage(
-      "Rechercher les points d\'intérêt et accéder aux services essentiels de la ville",
-    ),
-    "title2": MessageLookupByLibrary.simpleMessage(
-      "Exposer ses compétences, ses produits et services en ligne",
-    ),
-    "title3": MessageLookupByLibrary.simpleMessage(
-      "Trouver les infos pratiques et consulter l’agenda des manifestations",
-    ),
-    "tryAgain": MessageLookupByLibrary.simpleMessage("Reessayez"),
-    "username": MessageLookupByLibrary.simpleMessage("Nom d\'utilisateur"),
-  };
+        "addEmail": MessageLookupByLibrary.simpleMessage(
+          "Remplissez l\'adresse mail",
+        ),
+        "alreadyAccount": MessageLookupByLibrary.simpleMessage(
+          "J\'ai déja un compte",
+        ),
+        "appname": MessageLookupByLibrary.simpleMessage("Position"),
+        "askAccess": MessageLookupByLibrary.simpleMessage("Demandez l\'access"),
+        "close": MessageLookupByLibrary.simpleMessage("Fermé : "),
+        "closed": MessageLookupByLibrary.simpleMessage("Fermé"),
+        "confirmPassword": MessageLookupByLibrary.simpleMessage(
+          "Confirmer le Mot de Passe",
+        ),
+        "connexion": MessageLookupByLibrary.simpleMessage("Connexion"),
+        "cpassword": MessageLookupByLibrary.simpleMessage(
+          "Confirmer le Mot de Passe",
+        ),
+        "createAccount":
+            MessageLookupByLibrary.simpleMessage("Creer un compte"),
+        "email": MessageLookupByLibrary.simpleMessage("Adresse Mail"),
+        "emailNoSend": MessageLookupByLibrary.simpleMessage(
+          "Erreur de mail. Reessayez!!!",
+        ),
+        "emailSend": MessageLookupByLibrary.simpleMessage(
+          "Un mail de reinitialisation vous a été envoyé",
+        ),
+        "enableGps": MessageLookupByLibrary.simpleMessage(
+          "Assurez-vous d\'activer le GPS.",
+        ),
+        "enterEmailForReset": MessageLookupByLibrary.simpleMessage(
+          "Saisir l\'e-mail pour réinitialiser",
+        ),
+        "error":
+            MessageLookupByLibrary.simpleMessage("Une erreur est survenue"),
+        "forgotPass":
+            MessageLookupByLibrary.simpleMessage("Mot de Passe oublié ?"),
+        "go": MessageLookupByLibrary.simpleMessage("Allez-y"),
+        "gpsAccess": MessageLookupByLibrary.simpleMessage(
+          "Un accès GPS est nécessaire",
+        ),
+        "hintIdText": MessageLookupByLibrary.simpleMessage(
+          "Entrez votre identifiant ou votre email",
+        ),
+        "hintPasswordText": MessageLookupByLibrary.simpleMessage(
+          "Entrez votre mot de passe",
+        ),
+        "hintSearch": MessageLookupByLibrary.simpleMessage("Rechercher"),
+        "invalidId": MessageLookupByLibrary.simpleMessage(
+          "Téléphone ou Email Invalide",
+        ),
+        "invalidMail": MessageLookupByLibrary.simpleMessage("Email Invalide"),
+        "invalidPass": MessageLookupByLibrary.simpleMessage(
+          "Mot de Passe Invalide",
+        ),
+        "invalidPhone":
+            MessageLookupByLibrary.simpleMessage("Téléphone Invalide"),
+        "labelIdText":
+            MessageLookupByLibrary.simpleMessage("Identifiant/Email"),
+        "labelPasswordText":
+            MessageLookupByLibrary.simpleMessage("Mot de passe"),
+        "loading": MessageLookupByLibrary.simpleMessage("Chargement..."),
+        "loggin": MessageLookupByLibrary.simpleMessage("Connexion..."),
+        "login": MessageLookupByLibrary.simpleMessage("CONNECTEZ VOUS"),
+        "loginFailed":
+            MessageLookupByLibrary.simpleMessage("Echec de Connexion"),
+        "loginSuccess":
+            MessageLookupByLibrary.simpleMessage("Connexion reussie"),
+        "maintenance": MessageLookupByLibrary.simpleMessage(
+          "Votre application est en cours de maintenance. Reessayez plus tard",
+        ),
+        "next": MessageLookupByLibrary.simpleMessage("Suivant"),
+        "noAccount": MessageLookupByLibrary.simpleMessage(
+          "Pas encore de Compte?  ",
+        ),
+        "noInternet": MessageLookupByLibrary.simpleMessage(
+          "Pas de connexion internet",
+        ),
+        "open": MessageLookupByLibrary.simpleMessage("Ouvert"),
+        "opennow": MessageLookupByLibrary.simpleMessage("Ouvert Maintenant : "),
+        "or": MessageLookupByLibrary.simpleMessage("- OU -"),
+        "passError": MessageLookupByLibrary.simpleMessage(
+          "Mots de passes differents",
+        ),
+        "password": MessageLookupByLibrary.simpleMessage("Mot de Passe"),
+        "passwordReset": MessageLookupByLibrary.simpleMessage(
+          "réinitialisation du mot de passe",
+        ),
+        "passwordsDontMatch": MessageLookupByLibrary.simpleMessage(
+          "Les mots de passe ne correspondent pas",
+        ),
+        "phone": MessageLookupByLibrary.simpleMessage("Numéro de Téléphone"),
+        "register": MessageLookupByLibrary.simpleMessage("S\'INSCRIRE"),
+        "registerFailed": MessageLookupByLibrary.simpleMessage(
+          "Echec de création de compte",
+        ),
+        "registerSuccess": MessageLookupByLibrary.simpleMessage(
+          "Création du compte reussie verifiez vos mails...",
+        ),
+        "registering": MessageLookupByLibrary.simpleMessage(
+          "Création du compte...",
+        ),
+        "resetPassword": MessageLookupByLibrary.simpleMessage(
+          "Reinitialiser le Mot de Passe",
+        ),
+        "resetsuccess": MessageLookupByLibrary.simpleMessage(
+          "reinitialisation effectuée",
+        ),
+        "search": MessageLookupByLibrary.simpleMessage(
+          "Rechercher un établissement, un lieu...",
+        ),
+        "searcherror":
+            MessageLookupByLibrary.simpleMessage("Erreur de Chargement"),
+        "searchnotfound": MessageLookupByLibrary.simpleMessage(
+          "Aucun resultat trouvé",
+        ),
+        "sendResetLink": MessageLookupByLibrary.simpleMessage(
+          "Envoyer un lien de reinitialisation",
+        ),
+        "serverError": MessageLookupByLibrary.simpleMessage(
+          "Une erreur est survenue",
+        ),
+        "signwith": MessageLookupByLibrary.simpleMessage("Se Connecter avec"),
+        "skip": MessageLookupByLibrary.simpleMessage("Passer"),
+        "subtitle1": MessageLookupByLibrary.simpleMessage(
+          "Impression de plan officiel de localisation, partage, enregistrement dans ses favoris ou naviguation d\'un endroit vers un autre Pharmacie de garde, banque, centre de santé, écoles, infrastructures sociales et services publics",
+        ),
+        "subtitle2": MessageLookupByLibrary.simpleMessage(
+          "Une boutique virtuelle et un marché digital pour les activités formelles et informelles ...",
+        ),
+        "subtitle3": MessageLookupByLibrary.simpleMessage(
+          "Déclarations de Travaux, suivre l’actualité locale via les avis et annonces",
+        ),
+        "title1": MessageLookupByLibrary.simpleMessage(
+          "Rechercher les points d\'intérêt et accéder aux services essentiels de la ville",
+        ),
+        "title2": MessageLookupByLibrary.simpleMessage(
+          "Exposer ses compétences, ses produits et services en ligne",
+        ),
+        "title3": MessageLookupByLibrary.simpleMessage(
+          "Trouver les infos pratiques et consulter l’agenda des manifestations",
+        ),
+        "tryAgain": MessageLookupByLibrary.simpleMessage("Reessayez"),
+        "username": MessageLookupByLibrary.simpleMessage("Nom d\'utilisateur"),
+      };
 }
